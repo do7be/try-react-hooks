e011d41c4578c0165114fc1c10c1ecd3fb5dc6b5
do7be <bomsap700@gmail.com>
2018-11-03 10:51:00 +0900

    add password pattern

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+function generateNewPassword () {
+  return Math.random().toString(36).slice(-8)
+}
+
 function Example () {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0)
+  const [password, setPassword] = useState(generateNewPassword())
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
+    <>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+      <div>
+        <p>Your password is {password}</p>
+        <button onClick={() => setPassword(generateNewPassword())}>
+          Generate new password
+        </button>
+      </div>
+    </>
   )
 }
 
